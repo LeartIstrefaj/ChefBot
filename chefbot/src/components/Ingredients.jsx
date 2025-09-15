606f855a38c53e6460efd2245719b676e93356f5
Leart Istrefaj <leartistrefaj02@gmail.com>
2025-09-15 19:47:57 +0200

    added the Ingredients and Get recipe banner part

---
+++ chefbot/src/components/Ingredients.jsx
@@ -0,0 +1,19 @@
+export default function Ingredients(props) {
+    const ingredientsList = props.ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ));
+    return (
+        <section className="ingredients">
+            <h2>Ingredients on hand:</h2>
+            {/* {ingredientsList} */}
+            <ul className="ingredients-list">{ingredientsList}</ul>
+            <div className="get-recipe">
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button className="get-recipe-btn">Get a recipe</button>
+            </div>
+        </section>
+    );
+}
--- chefbot/src/includes/Index.jsx
+++ chefbot/src/includes/Index.jsx
@@ -1,10 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import Ingredients from '../components/Ingredients';
 export default function Index() {
+    const [ingredients, setIngredients] = useState(["test", "test2", "test3"]);
+    // const [recipes, setRecipes] = useState([]);
+
     return (
         <>
             <form action="#" className="form">
                 <input type="text" placeholder="Enter ingredients: e.g. Milk" className="form-control" />
-                <button className="btn" type="button">Add ingredients</button>
+                <button className="btn" type="button">Add ingredient</button>
             </form>
+            <Ingredients ingredients={ingredients}/>
+            {/* <Recipes /> */}
+            
         </>
     );
 }
