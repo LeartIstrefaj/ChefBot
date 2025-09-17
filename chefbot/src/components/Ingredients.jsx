0e9efefb8ed088b0cff94ceb4d28b813049a68bd
Leart Istrefaj <leartistrefaj02@gmail.com>
2025-09-17 11:54:10 +0200

    Adding the configs about calling the Ai API

--- chefbot/src/components/Ingredients.jsx
+++ chefbot/src/components/Ingredients.jsx
@@ -7,13 +7,13 @@ export default function Ingredients(props) {
             <h2>Ingredients on hand:</h2>
             {/* {ingredientsList} */}
             <ul className="ingredients-list">{ingredientsList}</ul>
-            <div className="get-recipe">
+            {props.ingredients.length > 3 && <div className="get-recipe">
                 <div>
                     <h3>Ready for a recipe?</h3>
                     <p>Generate a recipe from your list of ingredients.</p>
                 </div>
-                <button className="get-recipe-btn">Get a recipe</button>
-            </div>
+                <button className="get-recipe-btn" onClick={props.toggleRecipe}>Get a recipe</button>
+            </div>}
         </section>
     );
 }
--- chefbot/src/includes/Index.jsx
+++ chefbot/src/includes/Index.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Ingredients from '../components/Ingredients';
+import GenerateRecipe from '../components/GenerateRecipe';
 export default function Index() {
-    const [ingredients, setIngredients] = useState(["test", "test2", "test3"]);
-    // const [recipes, setRecipes] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
+    const [recipeShown, setRecipeShown] = useState(false);
+
+    const toggleRecipe = () => {
+        setRecipeShown(prevRecipe => !prevRecipe);
+    }
+
+
+    const addIngredient = (formData) => {
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
+    }
 
     return (
         <>
-            <form action="#" className="form">
-                <input type="text" placeholder="Enter ingredients: e.g. Milk" className="form-control" />
+            <form action={addIngredient} className="form">
+                <input type="text" placeholder="Enter ingredients: e.g. Milk" className="form-control" name='ingredient' required />
                 <button className="btn" type="button">Add ingredient</button>
             </form>
-            <Ingredients ingredients={ingredients}/>
-            {/* <Recipes /> */}
+
+            {ingredients.length > 0 && <Ingredients ingredients={ingredients} toggleRecipe={toggleRecipe} />}
+            {recipeShown && <GenerateRecipe />}
             
         </>
     );
